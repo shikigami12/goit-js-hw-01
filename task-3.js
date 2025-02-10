fd60dc7b0f748994f0ac03925ebe09535587c85e
Yevhen Khudoliiv <eugenkhudoliiv@gmail.com>
2025-02-10 19:49:01 +0100

    Add function to calculate element width with error handling
    Introduce `getElementWidth` function to compute the total element width based on content, padding, and border values. Includes error handling to manage invalid inputs and returns descriptive error messages. Added test cases to validate functionality.

---
+++ task-3.js
@@ -0,0 +1,18 @@
+function getElementWidth(content, padding, border) {
+    let result = 0;
+
+    try {
+        result = parseFloat(content) +
+            parseFloat(padding) * 2 +
+            parseFloat(border) * 2;
+    }
+    catch (e) {
+        return `Invalid input! Error: ${e.message}`;
+    }
+
+    return result;
+}
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
